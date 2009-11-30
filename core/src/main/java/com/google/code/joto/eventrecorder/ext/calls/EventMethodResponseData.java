5ca6a6ed07a76297b4dd09d042716ca5295b55b9
arnaud.nauwynck <arnaud.nauwynck@79033bb6-8c34-11de-b3ae-23bd1a17229c>
2009-11-30 20:58:39 +0000

    [JOTO-19] rename eventGenerator->eventWriter, changed packages, added JUnit tests

---
+++ core/src/main/java/com/google/code/joto/eventrecorder/ext/calls/EventMethodResponseData.java
@@ -0,0 +1,44 @@
+package com.google.code.joto.eventrecorder.ext.calls;
+
+import java.io.Serializable;
+
+/**
+ *
+ */
+public class EventMethodResponseData implements Serializable {
+
+	/** intenral for javA.io.Serializable */
+	private static final long serialVersionUID = 1L;
+	
+	private Object result;
+	private Throwable exception;
+	
+	// -------------------------------------------------------------------------
+	
+	public EventMethodResponseData() {
+	}
+	
+	public EventMethodResponseData(Object result, Throwable exception) {
+		this.result = result;
+		this.exception = exception;
+	}
+
+	// -------------------------------------------------------------------------
+
+	public Object getResult() {
+		return result;
+	}
+
+	public void setResult(Object p) {
+		this.result = p;
+	}
+
+	public Throwable getException() {
+		return exception;
+	}
+
+	public void setException(Throwable p) {
+		this.exception = p;
+	}
+	
+}
