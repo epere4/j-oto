6c558753340601f5f7c1f6c4b1fe1993f925c37f
arnaud.nauwynck@gmail.com <arnaud.nauwynck@gmail.com@79033bb6-8c34-11de-b3ae-23bd1a17229c>
2011-11-01 22:35:01 +0000

    added main for test

--- core/src/test/java/com/google/code/joto/testapp/FooPanelTest.java
+++ core/src/test/java/com/google/code/joto/testapp/FooPanelTest.java
@@ -9,6 +9,23 @@ import com.google.code.joto.util.io.ui.UiTestUtils;
 
 public class FooPanelTest {
 
+	public static void main(String[] args) {
+		JotoConfig config = new JotoConfig();
+		JotoContext context = new JotoContext(config);
+		
+		IFooService fooServiceImpl = new FooServiceImpl();
+		IFooService fooProxy = context.createDefaultMethodEventWriterProxy(fooServiceImpl);
+
+		context.getObjReplMap().addObjectInstanceReplacement(fooServiceImpl, "fooServiceImpl", null);
+		context.getObjReplMap().addObjectInstanceReplacement(fooProxy, "fooProxy", null);
+		
+		FooPanel panel = new FooPanel(fooProxy);
+		UiTestUtils.doShowInFrame(panel.getJComponent());
+		
+		JotoContextFacadePanel jotoFacadePanel = new JotoContextFacadePanel(context); 
+		UiTestUtils.doShowInFrame(jotoFacadePanel.getJComponent());
+	}
+	
 	@Test
 	public void testOpenFrame() {
 		JotoConfig config = new JotoConfig();
